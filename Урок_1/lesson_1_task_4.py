178f13317965abb7158554549e9f25ecc7084f6c
Aidar Khasanov <142885314+aidarhasan1984@users.noreply.github.com>
2023-12-07 23:55:05 +0500

    Add files via upload

---
+++ Урок_1/lesson_1_task_4.py
@@ -0,0 +1,4 @@
+first_name = input("Айдар: ")
+last_name = input("Хасанов: ")
+
+print("Вас зовут:", last_name, first_name)
---
+++ Урок_1/lesson_1_task_6.py
@@ -0,0 +1,4 @@
+def print_greeting():
+    print("Привет, Айдар!")
+
+print_greeting()
