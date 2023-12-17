4172f23460aa357350a0f6e1c50e6af9556efb1b
Aidar Khasanov <142885314+aidarhasan1984@users.noreply.github.com>
2023-12-17 14:26:43 +0500

    Add files via upload

---
+++ Урок_7/Задание 2_test.py
@@ -0,0 +1,50 @@
+import time
+
+class SlowCalculatorPage:
+    def __init__(self, browser):
+        self.browser = browser
+
+    def open(self, url):
+        self.browser.get(url)
+
+    def enter_delay(self, value):
+        delay_input = self.browser.find_element_by_id("delay")
+        delay_input.clear()
+        delay_input.send_keys(value)
+
+    def click_button_7(self):
+        self.browser.find_element_by_xpath("//span[text()='7']").click()
+
+    def click_button_plus(self):
+        self.browser.find_element_by_xpath("//span[text()='+']").click()
+
+    def click_button_8(self):
+        self.browser.find_element_by_xpath("//span[text()='8']").click()
+
+    def click_button_equals(self):
+        self.browser.find_element_by_xpath("//span[text()='=']").click()
+
+    def get_result_after_45_seconds(self):
+        time.sleep(45)
+        return self.browser.find_element_by_id("result").text
+
+import pytest
+from selenium import webdriver
+from pages import SlowCalculatorPage
+
+@pytest.fixture
+def browser():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+def test_slow_calculator(browser):
+    page = SlowCalculatorPage(browser)
+    page.open("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+    page.enter_delay(45)
+    page.click_button_7()
+    page.click_button_plus()
+    page.click_button_8()
+    page.click_button_equals()
+    result = page.get_result_after_45_seconds()
+    assert result == "15"
