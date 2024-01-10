51a3ae88cac100a6088c4a1ab666eddc8e3447f7
Aidar Khasanov <142885314+aidarhasan1984@users.noreply.github.com>
2024-01-10 14:57:27 +0500

    Delete Урок_7 directory

--- Урок_7/summary_page.py
+++
@@ -1,6 +0,0 @@
-class SummaryPage:
-    def __init__(self, browser):
-        self.browser = browser
-
-    def get_total(self):
-        return self.browser.find_element_by_class_name("summary_total_label").text
